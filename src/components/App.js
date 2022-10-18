9b6dab169c2f3f7c7c29e7005d59e3684df2f20c
ushasuray97 <50068044+ushasuray97@users.noreply.github.com>
2022-10-18 22:12:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,4 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-
+function para(){
+  ReactDom.render(<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>,document.getElementById('root'));
+                }
+export default para;
