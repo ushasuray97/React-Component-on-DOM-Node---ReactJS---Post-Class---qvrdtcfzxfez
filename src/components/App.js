897d950212b7d93d68776511cd7d8efad3300aa6
ushasuray97 <50068044+ushasuray97@users.noreply.github.com>
2022-10-21 19:01:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
+function App(){
 ReactDOM.render(
   <React.StrictMode>
     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
-export default;
+}
+export default App;
